fd9299326a41ee1b2797de35c6da7aae173b2385
Har4o <harihharalampiev@gmail.com>
2025-05-16 13:12:23 +0300

    Initial commit

---
+++ src/main/java/com/example/camp/model/Team.java
@@ -0,0 +1,25 @@
+package com.example.camp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "teams")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String teamColour;
+}
