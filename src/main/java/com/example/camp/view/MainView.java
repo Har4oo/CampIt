06f17141645575661cd5f5c31b8561daef11e506
Har4o <harihharalampiev@gmail.com>
2025-06-30 16:59:23 +0300

    Details changes

--- src/main/java/com/example/camp/view/MainView.java
+++ src/main/java/com/example/camp/view/MainView.java
@@ -1,28 +1,17 @@
 package com.example.camp.view;
 
-import com.example.camp.model.Camper;
 import com.example.camp.service.CamperService;
 import com.example.camp.service.TeamService;
 import com.example.camp.view.camper.CamperView;
-import com.example.camp.view.camper.TeamView;
-import com.vaadin.flow.component.Text;
+import com.example.camp.view.team.TeamView;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.sidenav.SideNav;
-import com.vaadin.flow.component.sidenav.SideNavItem;
-import com.vaadin.flow.dom.Style;
-import com.vaadin.flow.router.HighlightConditions;
-import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("home")
